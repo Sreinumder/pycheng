1f3b389dbd4a7ea9b642878ec73c879e9a8a9b33
Sreinumder <rojgosai02@gmail.com>
2024-02-17 02:08:26 +0545

    added color pallets

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 # Importing the library
 import pygame
+import json
 pygame.init()
 
 fps = 1
 clock = pygame.time.Clock()
-sc = 720
-screenDimension = (sc,sc*3/4)
+sc = 640 #Screen size
+screenDimension = (sc,int(sc*3/4))
+screen = pygame.display.set_mode((screenDimension[0],screenDimension[1]))
+
 boardDimension = int(screenDimension[1]*0.8)
 squareSize = boardDimension//8
 xOffset=(screenDimension[0]-boardDimension)//2
 yOffset=(screenDimension[1]-boardDimension)//2
-screen = pygame.display.set_mode((screenDimension[0],screenDimension[1]))
+
+# board color pallete and design
+with open('path_to_file/person.json') as f:
+    colorPalletes = json.load(f)
+print(colorPalletes)
+default_bg = pygame.image.load("assets/background/bg.jpg")
+#color pallete
+borderWidth = 4
 whiteCheck = (227,193,111)
 blackCheck = (184,139,74)
-borderColor = (21,21,21)
 black = (21,21,21)
 white = (222,217,182)
-screen.fill(blackCheck)
-for y in range(0,boardDimension,squareSize):
-    if y%(squareSize*2)==0: s = squareSize 
-    else: s = 0
-    for x in range(s,boardDimension,squareSize*2):
-        pygame.draw.rect(screen, whiteCheck, pygame.Rect(x+xOffset, y+yOffset, squareSize, squareSize))
-pygame.draw.rect(screen, borderColor, pygame.Rect(xOffset-1, yOffset-1, squareSize, squareSize))
-    
+def drawBoard(whiteSqColor=(96,104,108), blackSqColor=(44,47,49), Background=pygame.image.load("assets/background/bg.jpg"), borderSqColor=(21,21,21)):
+    screen.blit(Background, (0, 0))
+    for y in range(0,boardDimension,squareSize):
+        for x in range(0,boardDimension,squareSize):
+            if ((y+x-2)//squareSize)%2==0:
+                pygame.draw.rect(screen, whiteSqColor, pygame.Rect(x+xOffset, y+yOffset, squareSize, squareSize))
+            else:
+                pygame.draw.rect(screen, blackSqColor, pygame.Rect(x+xOffset, y+yOffset, squareSize, squareSize))
+    pygame.draw.rect(screen, borderSqColor, pygame.Rect(xOffset-1, yOffset-1, boardDimension+2, boardDimension+2), 1) 
+drawBoard() 
+# drawBoard(whiteCheck,blackCheck)
 pygame.display.flip()
 running = True
 while running:
